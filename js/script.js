290b6d76653c9b8a8b1c89e9705b11c74ae0bbf1
fujii-uu <uu.fujii@gmail.com>
2025-07-07 23:15:11 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+<script>
+  // カウントしたい数字をセット（例）
+  const todayVisitors = 35;       // 今日の訪問者数
+  const yesterdayVisitors = 10;   // 昨日の訪問者数
+
+  // IDで取得して数字をセット
+  document.getElementById('todayCount').textContent = todayVisitors;
+  document.getElementById('yesterdayCount').textContent = yesterdayVisitors;
+</script>
